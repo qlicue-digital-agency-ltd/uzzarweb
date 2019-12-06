8dc84b482c4e7dc3a7c71ed48f3bdc0b5fe1f070
admin <kalrobbynson@gmail.com>
2019-12-06 06:44:46 +0300

    shop table seeders completed

--- database/seeds/ShopsTableSeeder.php
+++ database/seeds/ShopsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Shop;
 use Illuminate\Database\Seeder;
 
 class ShopsTableSeeder extends Seeder
@@ -11,6 +12,92 @@ class ShopsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $shop = new Shop();
+        $shop->name = "PHAMARNCY";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "HARDWARE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "PEMBEJEO";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "SPARE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "SPORTS WARE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "LIQUOR STORE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "MINI SUPER MARKET";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "FURNITURE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "BAR";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "GENGE (SOKONI)";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "KIEPE (CHIPS)";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "HOME APPLIANCE";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "UREMBO & MANUKATO";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "SIMU";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "NGUO (MAVAZI)";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "RETAIL (BIDHAA ZA REJA REJA)";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "COMPUTER";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "VIATU";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "BABY-SHOP";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "JEWELS";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "BUTCHERY";
+        $shop->save();
+
+        $shop = new Shop();
+        $shop->name = "NGUO ZA NDANI ZA KIKE";
+        $shop->save();
     }
 }
